44fbc27d582aa3e2e9e867d18326328775c02e80
HaTien <havantien95tb@gmail.com>
2018-09-26 10:14:50 +0700

    bài tập về exception(xử lý ngoại lệ) điều kiện trong tam giác

---
+++ src/main/java/exception/Exception.java
@@ -0,0 +1,34 @@
+package exception;
+
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class Exception extends Throwable {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("nhap do dai canh a");
+        int a = sc.nextInt();
+        System.out.println("nhap do dai canh b");
+        int b = sc.nextInt();
+        System.out.println("nhap do dai canh c");
+        int c = sc.nextInt();
+        Exception calc = new Exception();
+        calc.calculate(a, b, c);
+
+    }
+
+        private void calculate(int a, int b, int c) throws Exception{
+        try {
+            if(a > 0 && b > 0 && c > 0){
+                System.out.println("dung");
+            } else if (a + b > c && a + c > b && c + b > a){
+                System.out.println("dung");
+            } else {
+                throw new Exception();
+            }
+
+        } catch (Exception e){
+            System.err.println("sai");
+        }
+    }
+}
